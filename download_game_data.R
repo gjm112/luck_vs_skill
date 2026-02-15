16b12d57ba1a010f1535f7f6da45d7b221ed1bae
sandycat27 <mosandov349@convexmail.com>
2026-02-15 10:25:45 -0600

    I think this is ready.

--- download_game_data.R
+++ download_game_data.R
@@ -12,28 +12,45 @@ determine_winner_scalar <- function(home_score, away_score) {
 }
 
 determine_winner <- Vectorize(determine_winner_scalar)
+nfl_cache_path <- "./nfl_play_by_play/"
 
-load_nfl_play_by_play <- function() {
+download_nfl_play_by_play <- function() {
   first_recorded_season <- 1999
   current_season <- 2025
-  cache_path <- "./nfl_play_by_play.rds"
-  if (file.exists(cache_path)) {
-    nfl_pbp <- readRDS(cache_path)
-  } else {
-    season_pbps <- list()
-    for (season_year in first_recorded_season:current_season) {
-      season_pbps[[season_year]] <- nflreadr::load_pbp(season_year)
+  for (season_year in first_recorded_season:current_season) {
+    file_path <- paste0(
+      nfl_cache_path, "play_by_play_", season_year, ".parquet"
+    )
+    if (!file.exists(file_path)) {
+      nflreadr::load_pbp(season_year) |>
+        as_tibble() |>
+        arrow::write_parquet(file_path)
     }
-    nfl_pbp <- dplyr::bind_rows(season_pbps)
-    saveRDS(nfl_pbp, cache_path)
   }
-  nfl_pbp
 }
 
+load_nfl_play_by_play <- function() {
+  nfl_rds_file <- "nfl_play_by_play.rds"
+  if (file.exists(nfl_rds_file)) {
+    pbps <- readRDS(nfl_rds_file)
+  } else {
+    files <- list.files(nfl_cache_path)
+    pbp_list <- list()
+    for (file in files) {
+      if (file.exists(file)) {
+        pbp_season <- arrow::read_parquet(file)
+        pbp_list <- append(pbp_list, pbp_season)
+      }
+    }
+    pbp <- bind_rows(pbp_list)
+    saveRDS(pbp |> as_tibble(), nfl_rds_file)
+  }
+  pbps
+}
 
 load_nfl_quarterly_scores <- function() {
-  nfl_pbp <- load_nfl_play_by_play()
   cache_path <- "./nfl_scores_by_quarter.rds"
+  nfl_pbp <- load_nfl_play_by_play()
   if (file.exists(cache_path)) {
     scores_per_quarter <- readRDS(cache_path)
   } else {
@@ -52,43 +69,56 @@ load_nfl_quarterly_scores <- function() {
   scores_per_quarter
 }
 
-load_mlb_play_by_play <- function() {
-  cache_path <- "./mlb_play_by_play.rds"
-  if (file.exists(cache_path)) {
-    mlb_pbp <- readRDS(cache_path)
-  } else {
-    search_dates <- seq(
-      ymd("2015-01-01"),
-      ymd("2026-01-01"),
-      by = "day"
-    ) |>
-      format("%Y-%m-%d")
-    seasons_pbps <- list()
-    for (date in search_dates) {
-      pbp_day <- baseballr::statcast_search(date, date)
-      seasons_pbps <- append(seasons_pbps, pbp_day)
-      print(pbp_day)
+mlb_cache_path <- "./mlb_statcast/"
+download_mlb_statcast <- function() {
+  search_dates <- seq(
+    ymd("2015-01-01"),
+    ymd("2026-01-01"),
+    by = "day"
+  ) |>
+    format("%Y-%m-%d")
+  for (date in search_dates) {
+    file_path <- paste0(mlb_cache_path, "gameday_", date, ".parquet")
+    if (!file.exists(file_path)) {
+      gameday <- baseballr::statcast_search(date, date) |> as_tibble()
+      if (nrow(gameday) > 0) {
+        arrow::write_parquet(gameday, file_path)
+      }
     }
-    mlb_pbp <- bind_rows(seasons_pbps)
-    saveRDS(mlb_pbp, cache_path)
   }
-  mlb_pbp
+}
+
+load_mlb_statcast <- function() {
+  mlb_rds_file <- "mlb_statscast.rds"
+  if (file.exists(mlb_rds_file)) {
+    games <- readRDS(mlb_rds_file)
+  } else {
+    files <- list.files(mlb_cache_path)
+    games_list <- list()
+    for (file in files) {
+      if (file.exists(file)) {
+        gameday <- arrow::read_parquet(file)
+        games_list <- append(games_list, gameday)
+      }
+    }
+    games <- bind_rows(games_list)
+    saveRDS(games |> as_tibble(), mlb_rds_file)
+  }
+  games
 }
 
 load_mlb_inning_scores <- function() {
-  mlb_pbp <- load_mlb_play_by_play()
+  mlb_games <- load_mlb_statcast()()
   cache_path <- "./mlb_scores_by_inning.rds"
   if (file.exists(cache_path)) {
     scores_per_inning <- readRDS(cache_path)
   } else {
-    scores_per_inning <- mlb_pbp |>
+    scores_per_inning <- mlb_games |>
       filter(game_type == "R") |>
-      group_by(game_pk, game_year, game_date, inning, home_team, away_team) |>
+      group_by(game_pk, game_date, inning, home_team, away_team) |>
       summarize(
-        home_score = max(home_score),
-        away_score = max(away_score),
-        post_home_score = max(post_home_score),
-        post_away_score = max(post_away_score)
+        home_score = max(post_home_score),
+        away_score = max(post_away_score)
       ) |>
       ungroup() |>
       mutate(yij = determine_winner(home_score, away_score)) |>
@@ -99,4 +129,4 @@ load_mlb_inning_scores <- function() {
   scores_per_inning
 }
 
-load_mlb_play_by_play()
+download_mlb_statcast()
