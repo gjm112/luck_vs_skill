aed9ac1c11b801c516a8f19654dfba44a03afe87
sandycat27 <mosandov349@convexmail.com>
2026-02-20 18:59:15 -0600

    Reworked the script. Cleanly produces RDS files now. The parquet files will be hosted on Google Drive as they large and numerous.

--- download_game_data.R
+++ download_game_data.R
@@ -12,8 +12,31 @@ determine_winner_scalar <- function(home_score, away_score) {
 }
 
 determine_winner <- Vectorize(determine_winner_scalar)
-nfl_cache_path <- "./nfl_play_by_play/"
 
+load_parquets_to_rds <- function(parquets_cache_path, rds_file) {
+  if (file.exists(rds_file)) {
+    pbp_rds_tibble <- readRDS(rds_file) |> as_tibble()
+    if (nrow(pbp_rds_tibble) == 0) {
+      stop("The reloaded RDS file has zero rows.")
+    }
+  } else {
+    parquet_files <- list.files(parquets_cache_path)
+    game_data_list <- map(parquet_files, function(file) {
+      parquet_file_path <- paste0(parquets_cache_path, "/", file)
+      arrow::read_parquet(parquet_file_path) |> as_tibble()
+    })
+
+    pbp_rds_tibble <- bind_rows(game_data_list)
+    if (nrow(pbp_rds_tibble) == 0) {
+      stop("The game PBP tibble created from parquets has zero rows.")
+    } else {
+      saveRDS(pbp_rds_tibble, rds_file)
+    }
+  }
+  pbp_rds_tibble
+}
+
+nfl_cache_path <- "./nfl_play_by_play/"
 download_nfl_play_by_play <- function() {
   first_recorded_season <- 1999
   current_season <- 2025
@@ -29,44 +52,19 @@ download_nfl_play_by_play <- function() {
   }
 }
 
-load_nfl_play_by_play <- function() {
-  nfl_rds_file <- "nfl_play_by_play.rds"
-  if (file.exists(nfl_rds_file)) {
-    pbps <- readRDS(nfl_rds_file)
-  } else {
-    files <- list.files(nfl_cache_path)
-    pbp_list <- list()
-    for (file in files) {
-      if (file.exists(file)) {
-        pbp_season <- arrow::read_parquet(file)
-        pbp_list <- append(pbp_list, pbp_season)
-      }
-    }
-    pbp <- bind_rows(pbp_list)
-    saveRDS(pbp |> as_tibble(), nfl_rds_file)
-  }
-  pbps
-}
-
-load_nfl_quarterly_scores <- function() {
+save_nfl_quarterly_scores <- function(nfl_pbp) {
   cache_path <- "./nfl_scores_by_quarter.rds"
-  nfl_pbp <- load_nfl_play_by_play()
-  if (file.exists(cache_path)) {
-    scores_per_quarter <- readRDS(cache_path)
-  } else {
-    scores_per_quarter <- nfl_pbp |>
-      filter(season_type == "REG") |>
-      group_by(game_id, season, game_date, qtr, home_team, away_team) |>
-      summarize(
-        home_score = max(total_home_score),
-        away_score = max(total_away_score)
-      ) |>
-      ungroup() |>
-      mutate(yij = determine_winner(home_score, away_score))
+  scores_per_quarter <- nfl_pbp |>
+    filter(season_type == "REG") |>
+    group_by(game_id, season, game_date, qtr, home_team, away_team) |>
+    summarize(
+      home_score = max(total_home_score),
+      away_score = max(total_away_score)
+    ) |>
+    ungroup() |>
+    mutate(yij = determine_winner(home_score, away_score))
 
-    saveRDS(scores_per_quarter, cache_path)
-  }
-  scores_per_quarter
+  saveRDS(scores_per_quarter, cache_path)
 }
 
 mlb_cache_path <- "./mlb_statcast/"
@@ -79,7 +77,7 @@ download_mlb_statcast <- function() {
     format("%Y-%m-%d")
   for (date in search_dates) {
     file_path <- paste0(mlb_cache_path, "gameday_", date, ".parquet")
-    print(file_path)
+
     if (!file.exists(file_path)) {
       gameday <- baseballr::statcast_search(date, date) |> as_tibble()
       if (nrow(gameday) > 0) {
@@ -89,45 +87,24 @@ download_mlb_statcast <- function() {
   }
 }
 
-load_mlb_statcast <- function() {
-  mlb_rds_file <- "mlb_statscast.rds"
-  if (file.exists(mlb_rds_file)) {
-    games <- readRDS(mlb_rds_file)
-  } else {
-    files <- list.files(mlb_cache_path)
-    games_list <- list()
-    for (file in files) {
-      if (file.exists(file)) {
-        gameday <- arrow::read_parquet(file)
-        games_list <- append(games_list, gameday)
-      }
-    }
-    games <- bind_rows(games_list)
-    saveRDS(games |> as_tibble(), mlb_rds_file)
-  }
-  games
+save_mlb_inning_scores <- function(mlb_statcast) {
+  file_to_compute <- "./mlb_scores_by_inning.rds"
+  scores_per_inning <- mlb_statcast |>
+    filter(game_type == "R") |>
+    group_by(game_pk, game_date, inning, home_team, away_team) |>
+    summarize(
+      home_score = max(post_home_score),
+      away_score = max(post_away_score)
+    ) |>
+    ungroup() |>
+    mutate(yij = determine_winner(home_score, away_score)) |>
+    arrange(game_pk, inning)
+
+  saveRDS(scores_per_inning, file_to_compute)
 }
 
-load_mlb_inning_scores <- function() {
-  mlb_games <- load_mlb_statcast()()
-  cache_path <- "./mlb_scores_by_inning.rds"
-  if (file.exists(cache_path)) {
-    scores_per_inning <- readRDS(cache_path)
-  } else {
-    scores_per_inning <- mlb_games |>
-      filter(game_type == "R") |>
-      group_by(game_pk, game_date, inning, home_team, away_team) |>
-      summarize(
-        home_score = max(post_home_score),
-        away_score = max(post_away_score)
-      ) |>
-      ungroup() |>
-      mutate(yij = determine_winner(home_score, away_score)) |>
-      arrange(game_pk, inning)
+mlb_statcast <- load_parquets_to_rds(mlb_cache_path, "mlb_statcast.rds")
+save_mlb_inning_scores(mlb_statcast)
 
-    saveRDS(scores_per_inning, cache_path)
-  }
-  scores_per_inning
-}
-
-download_mlb_statcast()
+nfl_pbp <- load_parquets_to_rds(nfl_cache_path, "nfl_play_by_play.rds")
+save_nfl_quarterly_scores(nfl_pbp)
